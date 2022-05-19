f73c9ef1536510aef1308c0eb87b4a8312a48385
Vrixic <vrijpatel123@icloud.com>
2022-05-19 10:16:09 -0400

    Adding SpawnPoint....

--- Assets/Scripts/Spawnables/SpawnPoint.cs
+++ Assets/Scripts/Spawnables/SpawnPoint.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//[ExecuteAlways]
-
 public class SpawnPoint : MonoBehaviour
 {
-    [SerializeField] GameObject prefab;
-    [SerializeField] Vector3 offset;
-
+    [SerializeField] GameObject m_Object;
     private ISpawnable spawnable;
 
-    
     private void Start()
     {
-        prefab = Instantiate(prefab, transform.position + offset, transform.rotation);
-        gameObject.SetActive(false);
+        spawnable = m_Object.GetComponentInChildren<ISpawnable>();
 
-        spawnable = prefab.GetComponentInChildren<ISpawnable>();
+        spawnable.SetInitialPosition(transform.position);
+        spawnable.SetInitialRotation(transform.rotation);
+    }
+
+    public virtual void Respawn()
+    {
+        gameObject.transform.position = Vector3.zero;
+        gameObject.transform.rotation = Quaternion.identity;
+        spawnable.Respawn();
     }
 }
