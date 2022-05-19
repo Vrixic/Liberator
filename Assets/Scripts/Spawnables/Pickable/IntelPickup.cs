11b655eae915521089cd628fc827f5038de484f6
Zomie432 <zachbeck432@gmail.com>
2022-05-19 13:40:30 -0400

    changed intel spawn, change level

---
+++ Assets/Scripts/Spawnables/Pickable/IntelPickup.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntelPickup : ISpawnable
+{
+    public override void Respawn()
+    {
+        base.Respawn();
+    }
+}
