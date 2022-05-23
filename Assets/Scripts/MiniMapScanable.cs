0a7e792e22716f475a06fd7bf725f86d7b8bc485
Zomie432 <zachbeck432@gmail.com>
2022-05-23 14:59:49 -0400

    minimap locator stuff

--- Assets/Scripts/MiniMapScanable.cs
+++ Assets/Scripts/MiniMapScanable.cs
@@ -6,19 +6,49 @@ public class MiniMapScanable : MonoBehaviour
 {
     [SerializeField]
     GameObject miniMapLocator;
+    Material locatorMaterial;
+
+    [SerializeField] float locationExposedTimer = 5f;
 
     private void Start()
     {
         miniMapLocator.SetActive(false);
+        locatorMaterial = miniMapLocator.GetComponent<Renderer>().material;
     }
 
     public void Show()
     {
         miniMapLocator.SetActive(true);
+        StartCoroutine(FlashLocator(locationExposedTimer));
     }
 
     public void Disable()
     {
         miniMapLocator.SetActive(false);
+    }
+
+    IEnumerator FlashLocator(float time)
+    {
+        bool fadingOut = true;
+        Color color = locatorMaterial.color;
+        color.a = 50;
+        while (time >= 0)
+        {
+            if (fadingOut)
+                color.a -= 4f;
+            else
+                color.a += 5f;
+
+            if (color.a < 0)
+                fadingOut = false;
+            if (color.a > 50)
+                fadingOut = true;
+
+            locatorMaterial.color = color;
+            time -= 0.05f;
+
+            yield return new WaitForSeconds(0.05f);
+        }
+        Disable();
     }
 }
--- Assets/Scripts/Weapon Scripts/Throwables/ScanGrenade.cs
+++ Assets/Scripts/Weapon Scripts/Throwables/ScanGrenade.cs
@@ -22,7 +22,7 @@ public class ScanGrenade : BaseThrowables
         PlayExplodeSFX();
 
         // Do Physics.OverlapSphereNonAlloc here
-        Collider[] colliders = new Collider[10];
+        Collider[] colliders = new Collider[20];
         Vector3 origin = transform.position;
         origin.y += 1f;
         int collidersCount = Physics.OverlapSphereNonAlloc(origin, scanSphereRadius, colliders, GetLayerMask());
@@ -37,15 +37,16 @@ public class ScanGrenade : BaseThrowables
             if (scanThroughWalls == false)
             {
                 Vector3 target = colliders[i].transform.position;
-                target.y += 0.5f; // so it doesn't target the ground
+                target.y += 0.25f; // so it doesn't target the ground
 
                 //Debug.DrawLine(origin, origin + (target - origin).normalized * raycastDistance, Color.green, 2f);
                 RaycastHit hitInfo;
                 if (Physics.Raycast(origin, (target - origin).normalized, out hitInfo, raycastDistance))
                 {
-                    if(hitInfo.collider.CompareTag("Hitbox"))
+                    if(hitInfo.collider.tag == "Hitbox")
                     {
-                        colliders[i].GetComponent<MiniMapScanable>().Show();
+                        Debug.Log("scannin: " + colliders[i].tag);
+                        hitInfo.collider.GetComponent<MiniMapScanable>().Show();
                     }                        
                 }
             }
