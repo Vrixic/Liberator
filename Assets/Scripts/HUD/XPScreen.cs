0a064f6ef18062ec74bae005bf1cbb8d7a21c3e3
Vrixic <vrijpatel123@icloud.com>
2022-06-06 09:08:09 -0400

    Fixed enemy display names.

--- Assets/Scripts/HUD/XPScreen.cs
+++ Assets/Scripts/HUD/XPScreen.cs
@@ -189,7 +189,19 @@ public class XPScreen : BaseScreen
             EnemyTextBlock block = new EnemyTextBlock();
             // Adds a new textholder to the scroll view, ands sets its appopriate names and kill amounts            
             block.holder = AddEnemyTextHolder();
-            block.holder.enemyNameText.text = pair.Key;
+            switch (pair.Key)
+            {
+                case "BasicEnemy":
+                    block.holder.enemyNameText.text = "Soldier";
+                    break;
+                case "MeleeUnit":
+                    block.holder.enemyNameText.text = "Stabber";
+                    break;
+                default:
+                    block.holder.enemyNameText.text = pair.Key;
+                    break;
+            }
+            
             block.holder.enemyKillText.text = "x" + pair.Value;
 
             block.transform = block.holder.GetComponent<RectTransform>();
