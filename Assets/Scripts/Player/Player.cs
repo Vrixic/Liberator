08ad44e8f0f58d61073a27e9771d2297e58e49c2
Vrixic21 <vrijpatel@icloud.com>
2022-06-13 13:16:26 -0400

    Pushed out updated player prefab

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -114,8 +114,8 @@ public class Player : MonoBehaviour
             //m_CurrentWeapons[i].OnPickup(weaponsParent);
         }
 
-        ActivateWeapon(0);
-        DeactivateWeapon(1);
+        //ActivateWeapon(0);
+        //DeactivateWeapon(1);
 
         // Set capcity of flashbangs and sensor grenades to Capacity dictated by Player Prefs
         flashbang.SetMaxAmountOfThrowables(PlayerPrefManager.Instance.flashBangCapacity);
@@ -141,6 +141,8 @@ public class Player : MonoBehaviour
 
         OnOptionsUpdate();
 
+        ActivateWeapon(0);
+        DeactivateWeapon(1);
     }
     void SetEquipmentEffectivness()
     {
@@ -237,30 +239,49 @@ public class Player : MonoBehaviour
         if (!GameRunningCheck()) return;
         if (!m_CurrentEquippedWeapon.CanSwitchWeapon()) return;
 
-        if (flashbang.isActiveAndEnabled)
+        //if (flashbang.isActiveAndEnabled)
+        //{
+        //    if (sensor.HasMoreThrowables())
+        //    {
+        //        DeactivateFlashbang();
+        //        ActivateSensor();
+        //    }
+        //}
+        //else
+        //{
+        //    if (flashbang.HasMoreThrowables())
+        //    {
+        //        DeactivateSensor();
+        //        DeactivateWeapon(m_CurrentWeaponIndex);
+        //        ActivateFlashbang();
+        //    }
+        //    else
+        //    {
+        //        if (sensor.HasMoreThrowables())
+        //        {
+        //            DeactivateWeapon(m_CurrentWeaponIndex);
+        //            ActivateSensor();
+        //        }
+        //    }
+        //}
+
+        if (flashbang.HasMoreThrowables())
         {
-            if (sensor.HasMoreThrowables())
-            {
-                DeactivateFlashbang();
-                ActivateSensor();
-            }
+         //   DeactivateSensor();
+            DeactivateWeapon(m_CurrentWeaponIndex);
+            ActivateFlashbang();
         }
-        else
+    }
+
+    public void EquipSensor()
+    {
+        if (!GameRunningCheck()) return;
+        if (!m_CurrentEquippedWeapon.CanSwitchWeapon()) return;
+
+        if (sensor.HasMoreThrowables())
         {
-            if (flashbang.HasMoreThrowables())
-            {
-                DeactivateSensor();
-                DeactivateWeapon(m_CurrentWeaponIndex);
-                ActivateFlashbang();
-            }
-            else
-            {
-                if (sensor.HasMoreThrowables())
-                {
-                    DeactivateWeapon(m_CurrentWeaponIndex);
-                    ActivateSensor();
-                }
-            }
+            DeactivateWeapon(m_CurrentWeaponIndex);
+            ActivateSensor();
         }
     }
 
@@ -387,12 +408,18 @@ public class Player : MonoBehaviour
     */
     private void ActivateFlashbang()
     {
+        if (sensor.isActiveAndEnabled)
+            DeactivateSensor();
+
         flashbang.SetActive(true);
         m_CurrentEquippedWeapon = flashbang;
     }
 
     private void ActivateSensor()
     {
+        if (flashbang.isActiveAndEnabled)
+            DeactivateFlashbang();
+
         sensor.SetActive(true);
         m_CurrentEquippedWeapon = sensor;
     }
